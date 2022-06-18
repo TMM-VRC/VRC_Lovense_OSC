ddbb0bdb2334e7d2d13721f0dcd944ecc4a85cf9
TMM-VRC <107766000+TMM-VRC@users.noreply.github.com>
2022-06-18 18:53:08 +0200

    Delete VRC_Lovense_OSC directory

--- VRC_Lovense_OSC/Start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set oscIP=0.0.0.0
-set oscPort=9001
-
-cd /D "%localappdata%/IntifaceDesktop/engine"
-start IntifaceCLI --servername "Intiface Server" --stayopen --wsinsecureport 12345 --with-lovense-connect
-cd /D %~dp0
-start buttplug-osc --osc-listen udp://%oscIP%:%oscPort%
